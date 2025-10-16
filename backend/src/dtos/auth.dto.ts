ad19e29434526770c4e37f051d48b1d734d02458
duykhangtong <khang.tong1124@hcmut.edu.vn>
2025-10-16 16:24:11 +0700

    feat(login): done

--- backend/src/dtos/auth.dto.ts
+++ backend/src/dtos/auth.dto.ts
@@ -1,11 +1,11 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { CreateUserDto } from "./user.dto";
-import { IsInt, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Length } from "class-validator";
 import { Type } from "class-transformer";
 
 export class RegisterDto extends CreateUserDto{
-    @ApiProperty({ 
-        example: '12', 
+    @ApiProperty({
+        example: '12',
         description: "existing address id, must have if user's role is staff",
         required: false
     })
@@ -14,8 +14,8 @@ export class RegisterDto extends CreateUserDto{
     @Type(() => Number)
     addressId?: number
 
-    @ApiProperty({ 
-        example: '12', 
+    @ApiProperty({
+        example: '12',
         description: "existing branch Id, must have if user's role is staff",
         required: false
     })
@@ -24,8 +24,8 @@ export class RegisterDto extends CreateUserDto{
     @Type(() => Number)
     branchId?: number
 
-    @ApiProperty({ 
-        example: '254 ABC street, ward 15, Go Vap, Ho Chi Minh city, Vietnam', 
+    @ApiProperty({
+        example: '254 ABC street, ward 15, Go Vap, Ho Chi Minh city, Vietnam',
         description: 'full address',
         required: false
     })
@@ -33,8 +33,8 @@ export class RegisterDto extends CreateUserDto{
     @IsString()
     addressText?: string
 
-    @ApiProperty({ 
-        example: '176.00013', 
+    @ApiProperty({
+        example: '176.00013',
         description: "Address's latitude",
         required: false
     })
@@ -43,8 +43,8 @@ export class RegisterDto extends CreateUserDto{
     @Type(() => Number)
     latitude?: number
 
-    @ApiProperty({ 
-        example: '176.00013', 
+    @ApiProperty({
+        example: '176.00013',
         description: "Address's longitude",
         required: false
     })
@@ -56,6 +56,30 @@ export class RegisterDto extends CreateUserDto{
 
 
 export class RegisterResponseDto {
+    @ApiProperty({ example: 'ey....', description: 'an accessToken for user token call guarded API'})
+    accessToken: string
+
+    @ApiProperty({ example: 'Nguyễn Văn A', description: "user full name for display somewhere"})
+    fullName: string
+
+    @ApiProperty({ example: 'client', description: "user's role"})
+    role: string
+}
+
+export class LoginReqDto {
+    @ApiProperty({ example: '0123456789', description: 'phone number to login' })
+    @IsString()
+    @IsNotEmpty()
+    @Length(10, 10, { message: 'Phone number must be exactly 10 characters' })
+    phone_number: string
+
+    @ApiProperty({ example: 'password123', description: 'password to login' })
+    @IsString()
+    @IsNotEmpty()
+    password: string
+}
+
+export class LoginResDto {
     @ApiProperty({ example: 'ey....', description: 'an accessToken for user token call guarded API'})
     accessToken: string
 
