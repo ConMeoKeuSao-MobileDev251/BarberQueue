3e9f5b7ed233e73a0d90bc6d9866db6e41958231
duykhangtong <khang.tong1124@hcmut.edu.vn>
2025-10-29 10:24:08 +0700

    feat(User): getById and update

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { UpdateUserDto, UserResponseDto } from 'src/dtos/user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('/:id')
+  @ApiOperation({summary: "get user by id"})
+  @ApiOkResponse({
+    description: "ok",
+    type: UserResponseDto
+  })
+  async getUserById(@Param('id', ParseIntPipe) id: number){
+    return await this.userService.getUserById(id);
+  }
+
+  @Put('/:id')
+  async updateUser(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDto){
+    return await this.userService.updateUser(id, body);
+  }
 }
